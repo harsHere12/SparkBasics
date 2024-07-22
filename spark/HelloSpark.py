023a139339df38b1ef8879cdb8d893913fb77403
harsHere12 <turkarharsh99@gmail.com>
2024-07-22 23:56:06 +0530

    create spark session with 3 local executer threads

--- spark/HelloSpark.py
+++ spark/HelloSpark.py
@@ -2,3 +2,10 @@ from pyspark.sql import SparkSession
 
 if __name__ == "__main__" :
     print("Hello Spark!")
+
+    spark = SparkSession.builder \
+            .appName("Hello Spark") \
+            .master("local[3]") \
+            .getOrCreate()
+    spark.stop()
+    print("Spark Session Stopped ")
