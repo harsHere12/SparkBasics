feb733979370f44e9fc4ee5753bd71d33a30d332
harsHere12 <turkarharsh99@gmail.com>
2024-07-23 00:10:20 +0530

    reading spark session config from spark.conf file

--- spark/HelloSpark.py
+++ spark/HelloSpark.py
@@ -1,11 +1,10 @@
 from pyspark.sql import SparkSession
-
+from lib.utils import get_spark_app_config
 if __name__ == "__main__" :
     print("Hello Spark!")
-
+    conf = get_spark_app_config()
     spark = SparkSession.builder \
-            .appName("Hello Spark") \
-            .master("local[3]") \
+            .config(conf=conf) \
             .getOrCreate()
     spark.stop()
     print("Spark Session Stopped ")
